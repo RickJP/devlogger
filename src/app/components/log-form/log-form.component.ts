a313d7db4e77248751d95e39bd400c962076ff87
Rick <rick@Ricks-MacBook-Pro.local>
2018-10-20 18:52:26 +0900

    Completed state & UX

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LogService } from '../../services/log.service';
 import { Log } from '../../models/Log';
-
+import { UUID } from 'angular2-uuid';
 
 
 
@@ -16,18 +16,55 @@ export class LogFormComponent implements OnInit {
   text: string;
   date: any;
 
+  isNew = true;
+
   constructor(private logService: LogService) { }
 
   ngOnInit() {
     // Subscribe to the selected log observable
     this.logService.selectedLog.subscribe(log => {
       if (log.id != null) {
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
+
       }
     });
+  }
 
+  onSubmit() {
+    // Check if new log
+    if (this.isNew) {
+      // Create a new log
+      const newLog = {
+        id: UUID.UUID(),
+        text: this.text,
+        date: new Date()
+      };
+      // Add log
+      this.logService.addLog(newLog);
+    } else {
+        // Create log to be updated
+        const updLog = {
+          id: this.id,
+          text: this.text,
+          date: new Date()
+        };
+        // Update log
+        this.logService.updateLog(updLog);
+    }
+
+     // Clear state
+  this.clearState();
+  }
+
+  clearState() {
+    this.isNew = true;
+    this.id = '';
+    this.text = '';
+    this.date = '';
+    this.logService.clearState();
   }
 
 }
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -13,12 +13,17 @@ export class LogService {
   private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
   selectedLog = this.logSource.asObservable();
 
+  private stateSource = new BehaviorSubject<boolean>
+  (true);
+  stateClear = this.stateSource.asObservable();
+
   constructor() {
-    this.logs = [
-      {id: '1', text: 'Generated components', date: new Date('10/26/2017 18:32:09')},
-      {id: '2', text: 'Added bootstrap', date: new Date('10/28/2017 13:12:09')},
-      {id: '3', text: 'Added logs complonent', date: new Date('10/29/2017 18:32:09')}
-    ];
+    // this.logs = [
+    //   {id: '1', text: 'Generated components', date: new Date('10/26/2017 18:32:09')},
+    //   {id: '2', text: 'Added bootstrap', date: new Date('10/28/2017 13:12:09')},
+    //   {id: '3', text: 'Added logs component', date: new Date('10/29/2017 18:32:09')}
+    // ];
+    this.logs = [];
    }
 
    getLogs(): Observable<Log[]> {
@@ -29,4 +34,29 @@ export class LogService {
      this.logSource.next(log);
    }
 
+
+    addLog(log: Log) {
+      this.logs.unshift(log);
+    }
+
+    updateLog(log: Log) {
+      this.logs.forEach((cur, index) => {
+        if (log.id === cur.id) {
+          this.logs.splice(index, 1);
+        }
+      });
+      this.logs.unshift(log);
+    }
+
+    deleteLog(log: Log) {
+      this.logs.forEach((cur, index) => {
+        if (log.id === cur.id) {
+          this.logs.splice(index, 1);
+        }
+      });
+    }
+
+    clearState() {
+      this.stateSource.next(true);
+    }
 }
